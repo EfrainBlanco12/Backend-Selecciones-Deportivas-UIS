93174d9d5ffb8cd6caf1a85ac888f7b9a6e58867
EfrainBlanco12 <efrainblancodpr@gmail.com>
2025-05-05 10:48:56 -0500

    Base del proyecto
    Base del proyector en spring boot

---
+++ src/main/java/com/deporuis/DeporuisApplication.java
@@ -0,0 +1,13 @@
+package com.deporuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeporuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeporuisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deporuis/DeporuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deporuis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeporuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
